3ae4f226f9480a81bd63baba4ffb624adc758d1c
leRoe93 <leon.roemer@icloud.com>
2021-09-15 20:36:09 +0200

    Created object that can be used as a really simple communication tunnel from agent back to the servlet.

---
+++ src/main/java/ntswwm/bean/AgentToServletStack.java
@@ -0,0 +1,14 @@
+package ntswwm.bean;
+
+import de.dfki.mycbr.core.casebase.Instance;
+
+/**
+ * This stack serves the purpose of enabling the agent providing information
+ * that a servlet can retrieve it from here.
+ * 
+ * @author leonr
+ *
+ */
+public class AgentToServletStack {
+    public static Instance FEEDBACK_INSTANCE;
+}
